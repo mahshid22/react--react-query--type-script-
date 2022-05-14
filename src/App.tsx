557188874b2e292bd9e9492ac37cfbd5756787d3
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-14 22:58:54 +0430

    add defaultOptions to react-query

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      cacheTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 function App() {
   return (
