fcd7fe8b9396b339f84758040ef86c7f87006b2f
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-16 23:06:01 +0430

    add logic to locations page

--- src/components/Locations/Locations.tsx
+++ src/components/Locations/Locations.tsx
@@ -1,13 +1,40 @@
 import React, { FC } from "react";
+import { useQueryClient } from "react-query";
+import { Link } from "react-router-dom";
+import { getLocation } from "../../hooks/location";
 import useLocations from "../../hooks/locations";
 import styles from "./Locations.module.css";
 
 interface LocationsProps {}
 
 const Locations: FC<LocationsProps> = () => {
+  const queryClient = useQueryClient();
   const { data, status } = useLocations();
-
-  return <div className={styles.Locations}>Locations Component</div>;
+  if (status !== "success") return <div>is Loading ...</div>;
+  return (
+    <div className={styles.Locations}>
+      <h2>Locations :</h2>
+      {data.results.map((location) => {
+        return (
+          <Link to={`/locations/${location.id}`}>
+            <p
+              onMouseEnter={async () => {
+                await queryClient.prefetchQuery(
+                  `location-${location.id}`,
+                  () => getLocation(Number(location.id)),
+                  {
+                    staleTime: 70 * 1000 /* 70 second */,
+                  }
+                );
+              }}
+            >
+              {location.name}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Locations;
