24621b0db45abf3e30657e499726caaf8364ae63
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-16 21:59:03 +0430

    add logic to characters and charatcer pages

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,13 +1,43 @@
 import React, { FC } from "react";
 import useCharacter from "../../hooks/character";
 import styles from "./Character.module.css";
+import { useParams } from "react-router";
+import useEpisode from "../../hooks/episode";
+import { stat } from "fs";
 
 interface CharacterProps {}
 
 const Character: FC<CharacterProps> = () => {
-  const characterId = null;
-  const { status, data, error, isFetching } = useCharacter(characterId);
-  return <div className={styles.Character}>Character Component</div>;
+  const { characterId } = useParams();
+  const { status, data, error, isFetching } = useCharacter(Number(characterId));
+  if (status !== "success") return <div>is Loading ...</div>;
+  return (
+    <div className={styles.Character}>
+      <h1>{data.name}</h1>
+      <p>Location : {data?.location.name}</p>
+      <p>Origin : {data?.origin.name}</p>
+      <p>Species : {data?.species}</p>
+      <p>Status : {data?.status}</p>
+      <p>Gender : {data?.gender}</p>
+      <div>
+        <h2>Episodes</h2>
+        {data.episode.map((eps) => {
+          const episodeUrlParts = eps.split("/").filter(Boolean);
+          const episodeId = episodeUrlParts[episodeUrlParts.length - 1];
+          return <Episode id={episodeId} key={`episode-${episodeId}`} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
+function Episode({ id }: { id: string }) {
+  const { status, data, error, isFetching } = useEpisode(Number(id));
+  if (status !== "success") return <div>is Loading ...</div>;
+  return (
+    <div>
+      {data.episode} - {data.name} - {data.air_date}
+    </div>
+  );
+}
 export default Character;
--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from "react";
+import { useQueryClient } from "react-query";
 import { Link } from "react-router-dom";
+import { getCharacter } from "../../hooks/character";
 import useCharacters from "../../hooks/characters";
 import styles from "./Characters.module.css";
 
 interface CharactersProps {}
 
 const Characters: FC<CharactersProps> = () => {
+  const queryClient = useQueryClient();
   const { data: Characters, status } = useCharacters();
+
   if (status !== "success")
     return <div className={styles.Characters}>is Loading ..</div>;
   return (
@@ -15,7 +19,17 @@ const Characters: FC<CharactersProps> = () => {
       {Characters.results.map((Character, index) => {
         return (
           <Link to={`/characters/${Character.url?.split("/")?.[5]}`}>
-            <p>
+            <p
+              onMouseEnter={async () => {
+                await queryClient.prefetchQuery(
+                  `character-${Character.url?.split("/")?.[5]}`,
+                  () => getCharacter(Number(Character.url?.split("/")?.[5])),
+                  {
+                    staleTime: 70 * 1000 /* 70 second */,
+                  }
+                );
+              }}
+            >
               {index + 1} - {Character.name} - {Character.gender}:{" "}
               {Character.species}
             </p>
