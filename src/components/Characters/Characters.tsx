3a541653a7d9f89378924cd789c494cc8521b168
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-16 15:12:04 +0430

    add login and some design to characters page

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,13 +1,29 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 import useCharacters from "../../hooks/characters";
 import styles from "./Characters.module.css";
 
 interface CharactersProps {}
 
 const Characters: FC<CharactersProps> = () => {
-  const { data, status } = useCharacters();
-
-  return <div className={styles.Characters}>Characters Component</div>;
+  const { data: Characters, status } = useCharacters();
+  if (status !== "success")
+    return <div className={styles.Characters}>is Loading ..</div>;
+  return (
+    <div className={styles.Characters}>
+      <h2>Character :</h2>
+      {Characters.results.map((Character, index) => {
+        return (
+          <Link to={`/characters/${Character.url?.split("/")?.[5]}`}>
+            <p>
+              {index + 1} - {Character.name} - {Character.gender}:{" "}
+              {Character.species}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Characters;
