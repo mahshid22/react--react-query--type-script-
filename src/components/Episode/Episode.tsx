5cce24a08ba452a9ea3fd96562a30520ce1669d3
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-16 21:59:26 +0430

    add logic to episodes and espisod pages

--- src/components/Episode/Episode.tsx
+++ src/components/Episode/Episode.tsx
@@ -1,14 +1,33 @@
 import React, { FC } from "react";
+import { useParams } from "react-router";
+import useCharacter from "../../hooks/character";
 import useEpisode from "../../hooks/episode";
 import styles from "./Episode.module.css";
 
 interface EpisodeProps {}
 
 const Episode: FC<EpisodeProps> = () => {
-  const episodeId = null;
-  const { status, data, error, isFetching } = useEpisode(episodeId);
+  const { episodeId } = useParams();
+  const { status, data, error, isFetching } = useEpisode(Number(episodeId));
+  if (status !== "success") return <div>is Loading...</div>;
+  return (
+    <div className={styles.Episode}>
+      <h2>{data.name}</h2>
+      <p>{data.air_date}</p>
 
-  return <div className={styles.Episode}>Episode Component</div>;
+      <h2>Characters</h2>
+      {data.characters.map((chr) => {
+        const chreUrlParts = chr.split("/").filter(Boolean);
+        const characterId = chreUrlParts[chreUrlParts.length - 1];
+        return <Character id={characterId} key={`chr-${characterId}`} />;
+      })}
+    </div>
+  );
 };
 
+function Character({ id }: { id: string }) {
+  const { status, data, error, isFetching } = useCharacter(Number(id));
+  if (status !== "success") return <div>is Loading</div>;
+  return <div>{data.name}</div>;
+}
 export default Episode;
--- src/components/Episodes/Episodes.tsx
+++ src/components/Episodes/Episodes.tsx
@@ -1,14 +1,42 @@
 import React, { FC } from "react";
 import { useQueryClient } from "react-query";
+import { Link } from "react-router-dom";
+import { getEpisode } from "../../hooks/episode";
 import useEpisodes from "../../hooks/episodes";
 import styles from "./Episodes.module.css";
 
 interface EpisodesProps {}
 
 const Episodes = () => {
-  const { data, status } = useEpisodes();
+  const { data: episodes, status } = useEpisodes();
+  const queryClient = useQueryClient();
 
-  return <div className={styles.Episodes}>Episodes Component</div>;
+  if (status !== "success")
+    return <div className={styles.Characters}>is Loading ..</div>;
+  return (
+    <div className={styles.Episodes}>
+      <h2>Episodes :</h2>
+      {episodes.results.map((episode, index) => {
+        return (
+          <Link to={`/episodes/${episode.url?.split("/")?.[5]}`}>
+            <p
+              onMouseEnter={async () => {
+                await queryClient.prefetchQuery(
+                  `episode-${episode.url?.split("/")?.[5]}`,
+                  () => getEpisode(Number(episode.url?.split("/")?.[5])),
+                  {
+                    staleTime: 70 * 1000 /* 70 second */,
+                  }
+                );
+              }}
+            >
+              {episode.episode} - {episode.name}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Episodes;
