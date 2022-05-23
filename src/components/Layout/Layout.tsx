a23bd0f8fab2b79efc0942af58b55208ad35f312
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-23 13:07:27 +0430

    add Route-based code splitting

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,16 @@
-import React, { FC } from "react";
+import React, { FC, lazy, Suspense } from "react";
 import Header from "../Header/Header";
 import styles from "./Layout.module.css";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
-import Character from "../Character/Character";
-import Characters from "../Characters/Characters";
-import Episode from "../Episode/Episode";
-import Episodes from "../Episodes/Episodes";
-import Home from "../Home/Home";
-import Footer from "../Footer/Footer";
-import Locations from "../Locations/Locations";
-import Location from "../Location/Location";
+import { Route, Routes } from "react-router-dom";
+
+const Character = lazy(() => import("../Character/Character"));
+const Characters = lazy(() => import("../Characters/Characters"));
+const Home = lazy(() => import("../Home/Home"));
+const Episodes = lazy(() => import("../Episodes/Episodes"));
+const Episode = lazy(() => import("../Episode/Episode"));
+const Footer = lazy(() => import("../Footer/Footer"));
+const Locations = lazy(() => import("../Locations/Locations"));
+const Location = lazy(() => import("../Location/Location"));
 
 interface LayoutProps {}
 
@@ -18,15 +19,20 @@ const Layout = () => (
     <div className={styles.LayoutMain}>
       <Header />
       <main>
-        <Routes>
-          <Route element={<Episodes />} path="/episodes"></Route>
-          <Route element={<Episode />} path="/episodes/:episodeId"></Route>
-          <Route element={<Characters />} path="/characters/:pageId"></Route>
-          <Route element={<Character />} path="/character/:characterId"></Route>
-          <Route element={<Locations />} path="/locations"></Route>
-          <Route element={<Location />} path="/locations/:locationId"></Route>
-          <Route element={<Home />} path="/"></Route>
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Episodes />} path="/episodes"></Route>
+            <Route element={<Episode />} path="/episodes/:episodeId"></Route>
+            <Route element={<Characters />} path="/characters/:pageId"></Route>
+            <Route
+              element={<Character />}
+              path="/character/:characterId"
+            ></Route>
+            <Route element={<Locations />} path="/locations"></Route>
+            <Route element={<Location />} path="/locations/:locationId"></Route>
+            <Route element={<Home />} path="/"></Route>
+          </Routes>
+        </Suspense>
       </main>
     </div>
 
